d6ab34f3834abdb286fc5085b98041eb48bdeba2
lazybytebr <mwar.aulas@gmail.com>
2025-08-12 11:06:13 +0000

    feat: Aula de 07/08/25

--- src/app/processa_news.php
+++ src/app/processa_news.php
@@ -17,24 +17,30 @@
 
     # string "texto" - number 10 1.5 - boolean true false - object - vetores - null 
     
-    # Recebemos o dado do formulário HTML e armazenamos na variável $email 
-    $email = $_POST['email'];
+# Recebemos o dado do formulário HTML e armazenamos na variável $email 
+$email = $_POST['email'];
 
     # echo "<h2> {$email} </h2>";
     # echo "<h3>" . $email . "</h3>";
     # echo '<h3>' . $email . '</h3>';
 
-    # Conectando ao Banco de Dados via PDO()
-    $conexao = new PDO( "mysql:host=db;dbname=estetica_t99;port=3306","root","root" );
+# 1. Conectando ao Banco de Dados via PDO()
+$conexao = new PDO( "mysql:host=db;dbname=estetica_t99;port=3306","root","root" );
 
-$sql_cria_tabela = "CREATE TABLE IF NOT EXISTS news ( 
-    id_news INT(255) NOT NULL DEFAULT AUTO_INCREMENT PRIMARY KEY,
+# 2. montar a instrução SQL
+$sql_cria_tabela = "CREATE TABLE IF NOT EXISTS estetica_t99.news ( 
+    id_news INT(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(254) NOT NULL UNIQUE,
     validado INT(1) NOT NULL DEFAULT 0,
+    opt_out ENUM('sim','não') NOT NULL DEFAULT 'sim',
     data_criacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 )";
 
+$sql_insere_email = "INSERT INTO estetica_t99.news ( email ) VALUES ( ? )";
 
+# 3. Executar o comando através da conexão
+$roda = $conexao->prepare( $sql_insere_email );
+$roda -> execute( [ $email ] );
 
 
 ?>
